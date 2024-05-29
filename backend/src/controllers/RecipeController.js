44cab797845f412c58b71b1ab7e04025affd27e3
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 21:49:57 +0530

    Added: `backend/src/controllers/RecipeController.js`

---
+++ backend/src/controllers/RecipeController.js
@@ -0,0 +1,60 @@
+import Recipe from '../models/Recipe';
+
+const RecipeController = {
+  getAllRecipes: async (req, res) => {
+    try {
+      const recipes = await Recipe.find();
+      res.status(200).json(recipes);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  getRecipeById: async (req, res) => {
+    try {
+      const recipe = await Recipe.findById(req.params.id);
+      if (!recipe) {
+        return res.status(404).json({ message: 'Recipe not found' });
+      }
+      res.status(200).json(recipe);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  createRecipe: async (req, res) => {
+    const newRecipe = new Recipe(req.body);
+    try {
+      const savedRecipe = await newRecipe.save();
+      res.status(201).json(savedRecipe);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  updateRecipe: async (req, res) => {
+    try {
+      const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!updatedRecipe) {
+        return res.status(404).json({ message: 'Recipe not found' });
+      }
+      res.status(200).json(updatedRecipe);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  deleteRecipe: async (req, res) => {
+    try {
+      const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id);
+      if (!deletedRecipe) {
+        return res.status(404).json({ message: 'Recipe not found' });
+      }
+      res.status(200).json({ message: 'Recipe deleted successfully' });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+};
+
+export default RecipeController;
